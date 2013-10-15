5c13d16fa0211e4b77cbdec4f4efb6ca8dbfbf6b
Sponis <sponis@gmail.com>
2013-10-16 00:38:43 +0300

    js added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
+jQuery(document).ready(function(){
 
+	jQuery('#mainmenu li').on('click',function(){
+		if (jQuery(this).hasClass("IsOpen")) {
+			jQuery(this).removeClass("IsOpen");
+			jQuery(this).find('.submenu').hide(500);
+		}
+		else {
+			jQuery('#mainmenu li .submenu').hide();
+			jQuery('#mainmenu li').removeClass("IsOpen");
+			jQuery(this).toggleClass("IsOpen");
+			jQuery(this).find('.submenu').show(500);
+		}
+	});
+
+
+
+
+
+
+
+});
